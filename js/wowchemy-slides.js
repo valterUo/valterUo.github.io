0e1522ce5f7b89176b5bcbc6e5d138965f3e8c28
Valter Uotila <valter.uotila@gmail.com>
2022-04-16 17:06:31 +0300

    Large update to everything

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = { highlight_style: "dracula", theme: "black" };
+  var slides = null;
 
   // ns-hugo:C:\Users\valte\AppData\Local\Temp\hugo_cache\modules\filecache\modules\pkg\mod\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5@v5.0.0-20220307125723-f79d02251851\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
